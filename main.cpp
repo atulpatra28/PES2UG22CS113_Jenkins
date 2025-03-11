b84c5140fde7a0a5f28f1d2cce706f11c84b031c
atulpatra28 <atulpatra28@gmail.com>
2025-03-12 01:02:00 +0530

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "Hello Jenkins, this is Atul bearing SRN PES2UG22CS113.\n";
+    cout << "I have successfully built and run this  \n";
+    return 0;
+}
