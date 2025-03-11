c2fe287423086ec22652d73b0a17c2a6a48805e3
atulpatra28 <atulpatra28@gmail.com>
2025-03-12 01:45:30 +0530

    u

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void)
 {
-    cout << "Hello Jenkins, this is Atul bearing SRN PES2UG22CS113.\n";
+    cout << "Hello Jenkins, I am Siri with SRN PES2UG22CS124!\n";
     cout << "I have successfully built and run this  \n";
     return 0;
 }
